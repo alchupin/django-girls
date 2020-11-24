8ddf4c32ffb264c51554005e048edbf56b85c0a3
Alexey <a.chupin@dm-solutions.ru>
2020-11-24 13:43:33 +0300

    view, template added

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v-y2a)rfzuidppgxere()-*uk-^(a0t$y747-g2@ehx_%*4z!^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'alchupin.pythonanywhere.com']
 
 
 # Application definition
